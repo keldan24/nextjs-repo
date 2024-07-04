3dddc2bda89f4b0e075c83e4346dbd6d0660b185
keldan24 <kelechidaniel171kd@gmail.com>
2024-07-04 17:16:20 +0100

    second commit

--- src/components/articleList.jsx
+++ src/components/articleList.jsx
@@ -1,5 +1,5 @@
-import ArticleItem from './ArticleItem'
-import articleStyles from '../styles/Article.module.css'
+import ArticleItem from './articleItem'
+import articleStyles from '../styles/article.module.css'
 
 const ArticleList = ({ articles }) => {
   return (
